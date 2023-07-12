b7687aad046ab104a02f3a3d065b96040fbb1c71
20424101-LeTuanKiet <20424101@student.hcmus.edu.vn>
2023-07-12 16:40:06 +0700

    fixed, playing the wrong song after searching

--- src/components/MusicList/Music/Music.js
+++ src/components/MusicList/Music/Music.js
@@ -15,7 +15,7 @@ export default function Music() {
                     className={cx('music')}
                     key={index}
                     onClick={() => {
-                        playMusic(index);
+                        playMusic(music.id);
                     }}
                 >
                     <div className={cx('music-id')}>{music.id}</div>
